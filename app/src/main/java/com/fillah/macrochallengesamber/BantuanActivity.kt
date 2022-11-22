494534e322e7aa0c149c9cc93089e35a3bf22a8d
ichcha <ichasng305@gmail.com>
2022-11-22 10:26:47 +0700

    Penambahan FaqActivity, dll

---
+++ app/src/main/java/com/fillah/macrochallengesamber/BantuanActivity.kt
@@ -0,0 +1,37 @@
+package com.fillah.macrochallengesamber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.back1
+import kotlinx.android.synthetic.main.activity_bantuan.*
+
+class BantuanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bantuan)
+        supportActionBar?.hide()
+
+        backButtonListener()
+        csListener()
+        faqListener()
+    }
+
+    private fun backButtonListener() {
+        back1.setOnClickListener {
+            startActivity(Intent(this, InitActivity::class.java))
+        }
+    }
+
+    private fun csListener(){
+        titleCs.setOnClickListener() {
+            startActivity(Intent(this, InitActivity::class.java))
+        }
+    }
+
+    private fun faqListener(){
+        titleFaq.setOnClickListener() {
+            startActivity(Intent(this, FaqActivity2::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/fillah/macrochallengesamber/FaqActivity.kt
@@ -0,0 +1,30 @@
+package com.fillah.macrochallengesamber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_faq.*
+import kotlinx.android.synthetic.main.activity_login.back1
+
+class FaqActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faq)
+        supportActionBar?.hide()
+
+    backButtonListener()
+    submitListener()
+}
+
+    private fun backButtonListener() {
+        back1.setOnClickListener {
+            startActivity(Intent(this, InitActivity::class.java))
+        }
+    }
+
+    private fun submitListener() {
+        submit.setOnClickListener {
+            startActivity(Intent(this, FaqActivity2::class.java))
+        }
+    }
+}
